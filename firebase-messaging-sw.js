4b9688cb70b25ed91fd6738bd250e7774eb9d33b
Qzief Dev <77611639+Qzief@users.noreply.github.com>
2025-10-31 22:00:48 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-messaging-compat.js');
 
+// Konfigurasi proyek Firebase kamu
 firebase.initializeApp({
   apiKey: "AIzaSyD9V0fG_bgxTcgcXQCfVLpvC-kjKeZBsiw",
   authDomain: "arufkuy-login-register.firebaseapp.com",
@@ -9,12 +10,16 @@ firebase.initializeApp({
   appId: "1:334457785991:web:a36fcfad9436c54f4b3222"
 });
 
+// Inisialisasi messaging
 const messaging = firebase.messaging();
 
+// Saat pesan diterima di background
 messaging.onBackgroundMessage((payload) => {
-  console.log('📩 Pesan background diterima: ', payload);
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
-    icon: '/icon.png' // opsional
+  console.log('📬 Pesan background:', payload);
+  const { title, body, icon } = payload.notification;
+
+  self.registration.showNotification(title, {
+    body,
+    icon: icon || 'https://cdn-icons-png.flaticon.com/512/1827/1827349.png'
   });
 });
