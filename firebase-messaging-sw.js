66a44a0671bceab8994522fd8f64ca122ca3f465
Qzief Dev <77611639+Qzief@users.noreply.github.com>
2025-10-31 21:58:31 +0800

    Refactor background message handling in service worker

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-messaging-compat.js');
 
-// Konfigurasi proyek Firebase kamu
 firebase.initializeApp({
   apiKey: "AIzaSyD9V0fG_bgxTcgcXQCfVLpvC-kjKeZBsiw",
   authDomain: "arufkuy-login-register.firebaseapp.com",
@@ -10,16 +9,12 @@ firebase.initializeApp({
   appId: "1:334457785991:web:a36fcfad9436c54f4b3222"
 });
 
-// Inisialisasi messaging
 const messaging = firebase.messaging();
 
-// Saat pesan diterima di background
 messaging.onBackgroundMessage((payload) => {
-  console.log('📬 Pesan background:', payload);
-  const { title, body, icon } = payload.notification;
-
-  self.registration.showNotification(title, {
-    body,
-    icon: icon || 'https://cdn-icons-png.flaticon.com/512/1827/1827349.png'
+  console.log('📩 Pesan background diterima: ', payload);
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: '/icon.png' // opsional
   });
 });
